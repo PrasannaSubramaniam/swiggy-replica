c60d0d13e1c31d6cf6c67deaa1484d0d6b29d669
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-12-19 17:21:13 +0530

    Repo Added

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // export const backendLink = "https://sleepy-springs-24187.herokuapp.com";
-export const backendLink = "https://swiggy-replica-express.herokuapp.com/";
+export const backendLink = "https://swiggy-replica-express.herokuapp.com";
 // export const backendLink = "http://localhost:3000";
