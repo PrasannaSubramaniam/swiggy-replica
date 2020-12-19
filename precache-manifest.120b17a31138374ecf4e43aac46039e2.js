d9d83f0b7f6796d29f6db82e2c6f028a6ec7d28d
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-12-19 17:30:15 +0530

    Updates

---
+++ precache-manifest.120b17a31138374ecf4e43aac46039e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba41ba43078375bb522c8ef314d8c89",
+    "url": "/swiggy-replica/index.html"
+  },
+  {
+    "revision": "06ece3e1ff5627ce61b9",
+    "url": "/swiggy-replica/static/css/main.bd6d5ac8.chunk.css"
+  },
+  {
+    "revision": "895074bc614e4e2e5f81",
+    "url": "/swiggy-replica/static/js/2.69e1953c.chunk.js"
+  },
+  {
+    "revision": "ff491b03ff6e95dcf1f3928fab3fbc10",
+    "url": "/swiggy-replica/static/js/2.69e1953c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ece3e1ff5627ce61b9",
+    "url": "/swiggy-replica/static/js/main.6ad8ea4a.chunk.js"
+  },
+  {
+    "revision": "e993d5a8a780283f4030",
+    "url": "/swiggy-replica/static/js/runtime-main.32cb39ff.js"
+  },
+  {
+    "revision": "112a7c200a9da858c373ae2f55405956",
+    "url": "/swiggy-replica/static/media/homepage.dd6eada4.jpeg"
+  }
+]);
